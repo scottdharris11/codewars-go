a91602de3677c40de3a4b49d13789e4eb3ad2c3e
scottdharris <scottdharris11@yahoo.com>
2021-01-23 14:26:07 -0600

    findodd puzzle

---
+++ findodd.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+func FindOdd(seq []int) int {
+	sort.Ints( seq )
+	previous := 0
+	count := 0
+	for idx, num := range seq {
+		if ( idx > 0 && previous != num ) {
+			if ( count % 2 == 1 ) {
+				return previous
+			}
+			count = 0
+		}
+		count++
+		previous = num
+	}
+    return previous
+}
